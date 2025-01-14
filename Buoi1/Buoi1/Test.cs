4d1247534c0cd73f7497382d13036de665aee203
huyvawn <63355258+huyvawn@users.noreply.github.com>
2025-01-14 20:29:10 +0700

    Add project files.

---
+++ Buoi1/Buoi1/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi1
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.WriteLine("Tien Huy");
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
